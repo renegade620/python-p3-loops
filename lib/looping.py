7f3057908b6942d239dd7edf48d6785a8d93a797
Frankline Were <franklinegift@gmail.com>
2024-08-29 10:26:45 +0300

    NY countdown

--- lib/looping.py
+++ lib/looping.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while (i > 0):
+        print(i)
+        i -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
@@ -11,3 +14,5 @@ def square_integers(int_list):
 def fizzbuzz():
     # code goes here!
     pass
+
+print(happy_new_year())
